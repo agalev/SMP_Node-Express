3e2d91eab8fd95f151600ad1230f843bea1dc4ef
Ash0x <alex.galev@yahoo.com>
2021-09-08 12:43:00 -0700

    added 2 GET routes

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,9 +2,33 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
+const DUMMY_PLACES = [
+  {id: 'p1',
+title: 'Empire State Building',
+description: 'One of the most famous skyscrapers in the world.',
+location: {
+  lat: 40.7484474,
+  lng: -73.9871516
+},
+address: '20 W 34th St, New York, NY 10001',
+creator: 'u1'}
+]
+
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid // { pid: 'p1' }
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeId
+  })
   console.log('GET Request in Places')
-  res.json({message: 'It works!'})
+  res.json({place})
+})
+
+router.get('/user/:uid', (req, res, next) => {
+  const userId = req.params.uid
+  const user = DUMMY_PLACES.find(u => {
+    return u.creator === userId
+  })
+  res.json({user})
 })
 
 module.exports = router
