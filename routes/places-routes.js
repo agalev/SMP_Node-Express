0777426ff4dec4d62f5e8d6fe558809d85e8c84f
Ash0x <alex.galev@yahoo.com>
2021-09-15 16:39:25 -0700

    Added /api/users routes for get all users, signup and login

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -6,8 +6,12 @@ const PlacesControllers = require('../controllers/places-controller')
 
 router.get('/:pid', PlacesControllers.getPlaceById)
 
-router.get('/user/:uid', PlacesControllers.getPlaceByUserId)
+router.get('/user/:uid', PlacesControllers.getPlacesByUserId)
 
 router.post('/', PlacesControllers.createPlace)
+
+router.patch('/:pid', PlacesControllers.updatePlace)
+
+router.delete('/:pid', PlacesControllers.deletePlace)
 
 module.exports = router
