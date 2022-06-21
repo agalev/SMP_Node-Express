519c7b27fda9ac79551cf7ab7a014fdff8354cc8
Ash0x <alex.galev@yahoo.com>
2022-06-21 10:32:34 -0700

    added PORT env

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(`mongodb+srv://alex:Password123321@cluster0.9nopx.mongodb.net/mern?retryWrites=true&w=majority`)
   .then(() => {
-    app.listen(5000)
+    app.listen(process.env.PORT || 5000)
   })
   .catch((err) => {
     console.log(err)
