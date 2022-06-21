7debb8b06ce562f34772c2b0ca47d8342ae60afc
Ash0x <alex.galev@yahoo.com>
2022-06-21 10:24:27 -0700

    fixing dependency issue

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use((error, req, res, next) => {
 })
 
 mongoose
-  .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9nopx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
+  .connect(`mongodb+srv://alex:Password123321@cluster0.9nopx.mongodb.net/mern?retryWrites=true&w=majority`)
   .then(() => {
     app.listen(5000)
   })
